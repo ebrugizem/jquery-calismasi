c60eef3cc56e0ce37782746b491d5ea8fdf407f9
Ebru Gizem Bozdoğan <118355588+ebrugizem@users.noreply.github.com>
2023-05-19 03:13:11 +0300

    admin.html eklendi

--- js/examp.js
+++ js/examp.js
@@ -13,7 +13,33 @@ $(document).ready(function(){
             alert ("Kulanıcı adı yanlış!!");
         }else {
             alert("Başarılıyla giriş yapıldı.")
+            $(location).prop('href','admin.html')
         } 
+    })
+    $(".npt").on("keypress", function(event) {
+        if (event.key === "Enter") {
+            var username,pass;
+            username=$("#frm #username").val();
+            pass=$("#frm #pass").val();
+            if(username== "" & pass ==""){ 
+                event.preventDefault();
+                alert("Kulanıcı adı ve Şifre boş bırakılamaz!!");  
+            }else if(pass == ""){
+                event.preventDefault();
+                alert("Şifre boş bırakılamaz!!");
+            } else if(username== "" ){
+                event.preventDefault();
+                alert ("Kulanıcı adı boş bırakılamaz!!");
+            }else if (username != "Admin"){
+                event.preventDefault();
+                alert ("Kulanıcı adı yanlış!!");
+            }else {
+                alert("Başarılıyla giriş yapıldı.")
+                $(location).prop('href','admin.html')
+            } 
+           
+        }
+        
     })
     
     $("#sub").on('click',function(){
@@ -24,6 +50,23 @@ $(document).ready(function(){
                 $("#city").val('');
                 $("#cList").append(`<li style="background-color:pink;">${city}</li>`);               
         }            
+     })
+    $("#city").on("keypress", function(event) {
+        if (event.key === "Enter") {
+        var city=$("#city").val();
+            if (city == "") {
+                    event.preventDefault();
+                    alert("Lüften bu alanı boş bırakmayınız")
+            }else{ 
+                  $("#city").val('');
+                  $("#cList").append(`<li style="background-color:pink;">${city}</li>`);               
+          } 
+        }
+        
+    })
+
+     $("#logOut").on("click",function(){
+        window.history.back();
      })
   
 });
